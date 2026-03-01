186c28d2299212b321210fe6f1610686dca63608
bandanakumariN <bandanakumarin99@gmail.com>
2026-03-01 18:57:03 +0530

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import myPhoto from 'https://github.com/bandanakumariN/bandanakumarin.github.io/blob/master/src/assets/images/bandana.png';
 import '../assets/styles/Main.scss';
 
 function Main() {
-
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src={myPhoto} alt="Avatar" />
+          <img 
+            src="https://raw.githubusercontent.com/bandanakumariN/bandanakumarin.github.io/master/src/assets/images/bandana.png" 
+            alt="Avatar" 
+          />
         </div>
         <div className="content">
           <div className="social_icons">
@@ -19,7 +20,6 @@ function Main() {
           </div>
           <h1>Bandana Kumari</h1>
           <p>Data Analyst</p>
-
           <div className="mobile_social_icons">
             <a href="https://github.com/bandanakumariN" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/bandana-kumarip/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
